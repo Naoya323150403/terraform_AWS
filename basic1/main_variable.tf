2cb29fba9bbd3a0e20ad7cc9d6f625195827c0c8
“Naoya323150403” <naoya.kansai.0403@gmail.com>
2022-12-21 00:38:42 +0900

    2022/12/20 basic1

---
+++ basic1/main_variable.tf
@@ -0,0 +1,9 @@
+variable "example_instance_type" {
+  default = "t3.micro"
+}
+
+resource "aws_instance" "example" {
+    ami           = "ami-0f9ae750e8274075b"
+    instance_type = "var.example_instance_type"
+
+}

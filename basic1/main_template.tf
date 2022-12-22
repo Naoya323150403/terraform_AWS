f576e79f28a75eb8abb039fbbbdd5bb8cbc52b56
“Naoya323150403” <naoya.kansai.0403@gmail.com>
2022-12-23 00:29:23 +0900

    2022/12/22 basic1

---
+++ basic1/main_template.tf
@@ -0,0 +1,17 @@
+data "template_file" "httpd_user_data"{
+  template = file("user_data.sh.tpl")
+
+  var = {
+    package = "httpd"
+  }
+}
+
+resource "aws_instance" "example" {
+  ami = "ami-0f9ae750e8274075b"
+  instance_type = "t3.micro"
+  user_data = data.template_file.httpd_user_data.rendered
+
+}
+
+
+
